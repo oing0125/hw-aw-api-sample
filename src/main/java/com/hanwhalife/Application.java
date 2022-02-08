ea8486229ed61741a62de6a5b53c5953db561081
oing0125 <bc.jung@hanwha.com>
2022-02-08 12:47:39 +0900

    Swagger 적용 완료

--- src/main/java/com/hanwhalife/Application.java
+++ src/main/java/com/hanwhalife/Application.java
@@ -1,7 +1,10 @@
 package com.hanwhalife;
 
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +12,5 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
